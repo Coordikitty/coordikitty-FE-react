9ac7743f26e7bd3a320741d2971f8c0bb7ed33ed
ches0703 <ches0703@naver.com>
2024-05-05 07:11:49 +0900

    SCRUM-129/[Feat] : 기본 라우팅 설정

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import router from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <RouterProvider router={router}></RouterProvider>
 );
 
