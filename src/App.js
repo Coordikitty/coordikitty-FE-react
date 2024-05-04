fe19ae30954115903ae8b0fed863fcc3ea7b7511
ches0703 <ches0703@naver.com>
2024-05-05 05:59:35 +0900

    SCRUM-128/[File] : 기본 파일 정리

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>This is jira test???</h1>
-        <h1>Jira branch Test</h1>
-        <h1>Jira branch Final Test</h1>
-      </header>
-    </div>
+    <div>Coordikitty</div>
   );
 }
 
