e2594f44c87025b50c441ee6edfb35b4103d9fcd
ches0703 <ches0703@naver.com>
2024-05-07 13:57:01 +0900

    SCRUM-93/[Feat] : 로그인 모달창 구현

--- src/components/Header/HeaderAuth.jsx
+++ src/components/Header/HeaderAuth.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { Stack, Button } from '@mui/material'
+import { useState } from 'react'
+import { 
+  Stack, 
+  Button, 
+} from '@mui/material'
+import SigninModal from './SigninModal'
+
 const HeaderAuth = () => {
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleModalOpen = () => setModalOpen(true);
+  const handleModalClose = () => setModalOpen(false);
+
   return (
-    <Stack direction={"row"} spacing={1}>
-      <Button variant="contained" fullWidth disableElevation>로그인</Button>
-      <Button variant="contained" fullWidth disableElevation>회워가입</Button>
-    </Stack>
+    <React.Fragment>
+      <Stack direction={"row"} spacing={1}>
+        <Button 
+          variant="contained" 
+          fullWidth 
+          disableElevation
+          onClick={handleModalOpen}
+        > 로그인
+        </Button>
+
+        <Button variant="contained" fullWidth disableElevation>회워가입</Button>
+      </Stack>
+      <SigninModal 
+        modalOpen={modalOpen} 
+        handleModalClose={handleModalClose}
+      ></SigninModal>
+    </React.Fragment>
   )
 }
 
