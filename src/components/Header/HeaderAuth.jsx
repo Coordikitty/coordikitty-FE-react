93d41e6c49eafa42e32a2fd0d60862409a483658
ches0703 <104610261+ches0703@users.noreply.github.com>
2024-05-10 11:15:16 +0900

    Revert "SCRUM-100 Merge"

--- src/components/Header/HeaderAuth.jsx
+++ src/components/Header/HeaderAuth.jsx
@@ -5,15 +5,12 @@ import {
   Button, 
 } from '@mui/material'
 import SigninModal from './SigninModal'
-import { useNavigate } from 'react-router-dom'
 
 const HeaderAuth = () => {
 
   const [modalOpen, setModalOpen] = useState(false);
   const handleModalOpen = () => setModalOpen(true);
   const handleModalClose = () => setModalOpen(false);
-
-  const navigate = useNavigate()
 
   return (
     <React.Fragment>
@@ -26,14 +23,7 @@ const HeaderAuth = () => {
         > 로그인
         </Button>
 
-        <Button 
-          variant="contained" 
-          fullWidth 
-          disableElevation
-          onClick={() => {navigate('/sign-up')}}
-        >
-          회원가입
-        </Button>
+        <Button variant="contained" fullWidth disableElevation>회워가입</Button>
       </Stack>
       <SigninModal 
         modalOpen={modalOpen} 
