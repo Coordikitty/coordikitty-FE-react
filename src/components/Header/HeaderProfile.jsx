b569cb579a9ee66ca1af2c07b96b5269e9bcbca0
ches0703 <ches0703@naver.com>
2024-08-01 18:26:25 +0900

    CD-222[Feat] : 헤더 디자인 변경

--- src/components/Header/HeaderProfile.jsx
+++ src/components/Header/HeaderProfile.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import { 
+import React, { useState } from 'react'
+import {
   Box,
   Avatar,
   Typography,
   Stack,
-  Button
+  Button,
+  Menu,
+  MenuItem,
+  IconButton,
+  useTheme
 } from '@mui/material'
 import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { logout } from '../../redux/UserReducer'
 import { useCookies } from 'react-cookie'
 
 const HeaderProfile = () => {
+  
+  const theme = useTheme()
+  const navigate = useNavigate()
 
-  const nickname = useSelector(state => state.user.nickname)
+  // const nickname = useSelector(state => state.user.nickname)
+  const nickname = 'Test'
   const dispatch = useDispatch()
   const [, , removeCookie] = useCookies()
+
+  // Menu
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = event => setAnchorEl(event.currentTarget)
+  const handleClose = () => setAnchorEl(null);
 
   const handleLogout = () => {
     dispatch(logout())
@@ -25,28 +40,65 @@ const HeaderProfile = () => {
   }
 
   return (
-    <Stack direction={"row"} alignItems={'center'}>
+    <Stack direction={"row"} alignItems={'center'} justifyContent={'flex-end'}>
       {/* Profile Img */}
-      <Avatar alt='user' sx={{width: "5rem", height: "5rem"}}></Avatar>
-      <Box width={"100%"}>
-        <Stack spacing={1}>
-          {/* User nickname */}
-          <Stack direction={'row'} justifyContent={'space-between'} paddingLeft={"2rem"} >
-            <Typography variant='h3'>{nickname}</Typography>
-            <Button variant='outlined' color='black' size='small'
-              onClick={handleLogout}
-            >
-              로그아웃
-            </Button>
-          </Stack>
-          {/* User info */}
-          <Stack direction={'row'} justifyContent={'space-between'} textAlign={'center'} padding={'0 2rem'}>
+      <IconButton onClick={handleClick}>
+        <Avatar alt='user' sx={{ width: "5rem", height: "5rem" }}></Avatar>
+
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        slotProps={{
+          paper: {
+            sx: {
+              borderRadius: '0.75rem',
+              padding: '0.5rem 1rem'
+            }
+          }
+        }}
+      >
+        {/* User info */}
+        <Stack spacing={1} textAlign={'end'}>
+          <Typography variant='h3'>User</Typography>
+          <Stack direction={'row'} spacing={2} justifyContent={'space-between'} textAlign={'center'}>
             <Typography variant='body2'>게시물 : {1}</Typography>
             <Typography variant='body2'>팔로워 : {1}</Typography>
             <Typography variant='body2'>팔로잉 : {1}</Typography>
           </Stack>
+          <MenuItem 
+            onClick={() => {
+              handleClose()
+              navigate('/my-page')}
+            }
+            disableGutters 
+          >
+            My Page
+          </MenuItem>
+          <MenuItem 
+            onClick={() => {
+              handleClose()
+              handleLogout()
+            }}
+            disableGutters 
+            sx={{ 
+              color: theme.palette.error.main,
+            }}
+          >
+            Logout
+          </MenuItem>
         </Stack>
-      </Box>
+      </Menu>
+
     </Stack>
   )
 }
