3af302f57e7251ac4d23ea7a13abf0cf812f9183
ches0703 <ches0703@naver.com>
2024-08-15 22:14:26 +0900

    [Feat] : 라우팅 방식 수정

--- src/router.js
+++ src/router.js
@@ -1,4 +1,8 @@
-import { createBrowserRouter, createHashRouter, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  Navigate,
+} from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
@@ -8,7 +12,7 @@ import Mypage from "./pages/Mypage";
 import Guard from "./pages/Guard";
 import PostPage from "./pages/PostPage";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
@@ -16,43 +20,49 @@ const router = createHashRouter([
       // Home page ---------------------
       {
         index: true,
-        element: <Home></Home>
+        element: <Home></Home>,
       },
       // -------------------------------
 
       // Post --------------------------
-      {// Posts page
+      {
+        // Posts page
         path: "posts",
-        element: <PostPage></PostPage>
+        element: <PostPage></PostPage>,
       },
-      {// Post page
+      {
+        // Post page
         path: "post/:postId",
-        element: <Post></Post>
+        element: <Post></Post>,
       },
       // postId가 없는 경우에 대한 리다이렉트 설정
       {
         path: "post/",
-        element: <Navigate to="/posts" replace />
+        element: <Navigate to="/posts" replace />,
       },
-      {// Post Write page
+      {
+        // Post Write page
         path: "post-write",
-        element: <Guard><PostWrite></PostWrite></Guard>
+        element: (
+          <Guard>
+            <PostWrite></PostWrite>
+          </Guard>
+        ),
       },
-      // ------------------------------- 
+      // -------------------------------
 
       // Account------------------------
       {
         path: "sign-up",
-        element: <Signup></Signup>
+        element: <Signup></Signup>,
       },
       {
         path: "my-page",
-        element: <Mypage></Mypage>
-      }
-      // ------------------------------- 
-    
-    ]
-  }
-])
+        element: <Mypage></Mypage>,
+      },
+      // -------------------------------
+    ],
+  },
+]);
 
-export default router
+export default router;
